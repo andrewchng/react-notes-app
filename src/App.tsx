3c30d2a58f649a6562ae5a95ed15c2a7055710a1
andrewchng92 <andrewchng92@gmail.com>
2024-08-03 16:26:21 +0800

    added basic skeleton

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,74 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import SideBar from "./components/Sidebar/Sidebar";
+import Editor from "./components/Editor/Editor";
+import { nanoid } from 'nanoid'
+
+export type Note = {
+  body?: string;
+  title: string;
+  id: string;
+  // isActive : boolean;
+};
+
+function seedNotes() {
+  const notes: Note[] = [
+    { title: "hello", body: "body", id: nanoid()},
+    { title: "hello2", body: "body", id: nanoid()},
+    { title: "hello3", body: "body", id: nanoid()},
+    { title: "hello4", body: "body", id: nanoid()},
+    { title: "hello", body: "body", id: nanoid()},
+    { title: "hello2", body: "body", id: nanoid()},
+    { title: "hello3", body: "body", id: nanoid()},
+    { title: "hello4", body: "body", id: nanoid()},
+    { title: "hello", body: "body", id: nanoid()},
+    { title: "hello2", body: "body", id: nanoid()},
+    { title: "hello3", body: "body", id: nanoid()},
+    { title: "hello4", body: "body", id: nanoid()},
+    { title: "hello", body: "body", id: nanoid()},
+    { title: "hello2", body: "body", id: nanoid()},
+    { title: "hello3", body: "body", id: nanoid()},
+    { title: "hello4", body: "body", id: nanoid()},
+    { title: "hello", body: "body", id: nanoid()},
+    { title: "hello2", body: "body", id: nanoid()},
+    { title: "hello3", body: "body", id: nanoid()},
+    { title: "hello4", body: "body", id: nanoid()},
+    { title: "hello", body: "body", id: nanoid()},
+    { title: "hello2", body: "body", id: nanoid()},
+    { title: "hello3", body: "body", id: nanoid()},
+    { title: "hello4", body: "body", id: nanoid()},
+    { title: "hello", body: "body", id: nanoid()},
+    { title: "hello2", body: "body", id: nanoid()},
+    { title: "hello3", body: "body", id: nanoid()},
+    { title: "hello4", body: "body", id: nanoid()},
+    { title: "hello", body: "body", id: nanoid()},
+    { title: "hello2", body: "body", id: nanoid()},
+    { title: "hello3", body: "body", id: nanoid()},
+    { title: "hello4", body: "body", id: nanoid()},
+  ];
+
+  return notes;
+}
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [notes, setNotes] = useState(seedNotes());
+  const [activeNote, setActiveNote] = useState(notes[0]);
 
   return (
     <>
-    <h1 className='text-4xl'>21</h1>
+      <div className="grid grid-cols-3 h-dvh">
+        <div className="sidebar-container col-span-1">
+          <SideBar setActiveNote={setActiveNote} notes={notes}></SideBar>
+        </div>
+        <div className="editor-container col-span-2">
+          <Editor></Editor>
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
+
+
---
+++ src/components/Sidebar/Sidebar.tsx
@@ -0,0 +1,26 @@
+import "./Sidebar.css"
+import { Note } from "../../App";
+
+type SideBarProps = {
+    notes: Note[];
+    setActiveNote: Function
+}
+
+const SideBar = ({ notes, setActiveNote } : SideBarProps) => {
+  return (
+    <div className="sidebar">
+      <div className="h-10 bg-blue-300">
+        <h1>icons for add</h1>
+      </div>
+      <div className="notes-list">
+        {notes.map(note => (
+          <div onClick={setActiveNote(note)} key={note.id} className="note">
+            <h3 className="title">{note.title}</h3>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default SideBar
