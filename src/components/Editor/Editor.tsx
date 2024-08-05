d05e8ab45de52af619c925408e110197344bb050
andrewchng92 <andrewchng92@gmail.com>
2024-08-05 23:29:57 +0800

    clean up

--- src/components/Editor/Editor.tsx
+++ src/components/Editor/Editor.tsx
@@ -3,11 +3,11 @@ import { note } from "../../App";
 import ReactMde from "react-mde";
 import * as Showdown from "showdown";
 import "react-mde/lib/styles/css/react-mde-all.css";
-import "./Editor.css"
+import "./Editor.css";
 
 type EditorProps = {
   currentNote: note | undefined;
-  updateNote : Function
+  updateNote: Function;
 };
 
 const converter = new Showdown.Converter({
@@ -26,18 +26,17 @@ export default function Editor({ currentNote, updateNote }: EditorProps) {
           <ReactMde
             minEditorHeight={94.5}
             maxEditorHeight={94.5}
-            minPreviewHeight={85.5}
             heightUnits="vh"
-            classes={ {"textArea" : "apply-color", "toolbar" : "apply-color"}}
+            classes={{ textArea: "apply-color", toolbar: "apply-color" }}
             value={currentNote.body}
-            onChange={(value =>{
-                updateNote(value);
-       })}
+            onChange={(value) => {
+              updateNote(value);
+            }}
             selectedTab={selectedTab as "write" | "preview"}
             onTabChange={setSelectedTab}
             generateMarkdownPreview={(markdown) =>
               Promise.resolve(converter.makeHtml(markdown))
-            }     
+            }
           />
         </div>
       )}
